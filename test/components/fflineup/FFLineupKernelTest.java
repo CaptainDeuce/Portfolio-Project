5f018359a11e464eae60c0e8b8482667a8fb069c
CaptainDeuce <deuceboes@gmail.com>
2024-11-21 18:40:24 -0500

    did most secondary tests, started kernel tests

--- test/components/fflineup/FFLineupKernelTest.java
+++ test/components/fflineup/FFLineupKernelTest.java
@@ -1,9 +1,48 @@
 package components.fflineup;
 
-/**
- * JUnit test fixture for {@code FFLineup}'s constructor, kernel methods, and
- * the Standard methods.
- */
-public class FFLineupKernelTest {
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
+/**
+ * JUnit test fixture for {@code FFLineup}'s kernel methods and the Standard
+ * methods.
+ */
+public abstract class FFLineupKernelTest {
+
+    /**
+     * Testing the addPlayer method with 1 QB.
+     *
+     * @param p
+     *            the player
+     */
+    @Test
+    public void testAddPlayer1QB(Player p) {
+        FFLineup f = new FFLineupOnArrayList();
+        FFLineup fCopy = new FFLineupOnArrayList();
+
+        f.addPlayer(p);
+        fCopy.addPlayer(p);
+
+        assertEquals(fCopy, f);
+    }
+
+    /**
+     * Testing the removePlayer method with 1 QB.
+     *
+     * @param p
+     *            the player
+     */
+    @Test
+    public void testRemovePlayer1QB(Player p) {
+        FFLineup f = new FFLineupOnArrayList();
+        FFLineup fCopy = new FFLineupOnArrayList();
+
+        f.addPlayer(p);
+        fCopy.addPlayer(p);
+        f.removePlayer(p);
+        fCopy.removePlayer(p);
+
+        assertEquals(fCopy, f);
+    }
 }
--- test/components/fflineup/FFLineupTest.java
+++ test/components/fflineup/FFLineupTest.java
@@ -10,6 +10,26 @@ import org.junit.Test;
 public abstract class FFLineupTest {
 
     /**
+     * Testing the returnPosition method with 0 TEs.
+     *
+     * @param s
+     *            the position
+     */
+    @Test
+    public void testReturnPosition0TE(String s) {
+        FFLineup f = new FFLineupOnArrayList();
+        FFLineup fCopy = new FFLineupOnArrayList();
+        Player jalenHurts = new Player("Jalen Hurts", "QB", null);
+        f.addPlayer(jalenHurts);
+        fCopy.addPlayer(jalenHurts);
+
+        FFLineup returned = f.returnPosition(s);
+        FFLineup returnedCopy = fCopy.returnPosition(s);
+        assertEquals(returnedCopy, returned);
+        assertEquals(fCopy, f);
+    }
+
+    /**
      * Testing the returnPosition method with 1 QB.
      *
      * @param s
@@ -74,6 +94,97 @@ public abstract class FFLineupTest {
 
         FFLineup returned = f.returnPosition(s);
         FFLineup returnedCopy = fCopy.returnPosition(s);
+        assertEquals(returnedCopy, returned);
+        assertEquals(fCopy, f);
+    }
+
+    /**
+     * Testing the returnPoints method with no players matching the points.
+     *
+     * @param d
+     *            the points
+     */
+    @Test
+    public void testReturnPointsNoMatch(double d) {
+        FFLineup f = new FFLineupOnArrayList();
+        FFLineup fCopy = new FFLineupOnArrayList();
+        final double points = 21.23;
+        Player jalenHurts = new Player("Jalen Hurts", "QB", points);
+        f.addPlayer(jalenHurts);
+        fCopy.addPlayer(jalenHurts);
+
+        FFLineup returned = f.returnPoints(d);
+        FFLineup returnedCopy = fCopy.returnPoints(d);
+        assertEquals(returnedCopy, returned);
+        assertEquals(fCopy, f);
+    }
+
+    /**
+     * Testing the returnPoints method with 1 QB.
+     *
+     * @param d
+     *            the points
+     */
+    @Test
+    public void testReturnPoints1QB(double d) {
+        FFLineup f = new FFLineupOnArrayList();
+        FFLineup fCopy = new FFLineupOnArrayList();
+        Player jalenHurts = new Player("Jalen Hurts", "QB", d);
+        f.addPlayer(jalenHurts);
+        fCopy.addPlayer(jalenHurts);
+
+        FFLineup returned = f.returnPoints(d);
+        FFLineup returnedCopy = fCopy.returnPoints(d);
+        assertEquals(returnedCopy, returned);
+        assertEquals(fCopy, f);
+    }
+
+    /**
+     * Testing the returnPoints method with 2 RBs.
+     *
+     * @param d
+     *            the points
+     */
+    @Test
+    public void testReturnPoints2RB(double d) {
+        FFLineup f = new FFLineupOnArrayList();
+        FFLineup fCopy = new FFLineupOnArrayList();
+        Player bijanRobinson = new Player("Bijan Robinson", "RB", d);
+        Player devonAchane = new Player("De'von Achane", "RB", d);
+        f.addPlayer(bijanRobinson);
+        fCopy.addPlayer(bijanRobinson);
+        f.addPlayer(devonAchane);
+        fCopy.addPlayer(devonAchane);
+
+        FFLineup returned = f.returnPoints(d);
+        FFLineup returnedCopy = fCopy.returnPoints(d);
+        assertEquals(returnedCopy, returned);
+        assertEquals(fCopy, f);
+    }
+
+    /**
+     * Testing the returnPoints method with 3 WRs.
+     *
+     * @param d
+     *            the points
+     */
+    @Test
+    public void testReturnPoints3WR(double d) {
+        FFLineup f = new FFLineupOnArrayList();
+        FFLineup fCopy = new FFLineupOnArrayList();
+        final double points = 22.4;
+        Player tankDell = new Player("Tank Dell", "WR", d);
+        Player nicoCollins = new Player("Nico Collins", "WR", points);
+        Player cedricTillman = new Player("Cedric Tillman", "WR", d);
+        f.addPlayer(tankDell);
+        fCopy.addPlayer(tankDell);
+        f.addPlayer(nicoCollins);
+        fCopy.addPlayer(nicoCollins);
+        f.addPlayer(cedricTillman);
+        fCopy.addPlayer(cedricTillman);
+
+        FFLineup returned = f.returnPoints(d);
+        FFLineup returnedCopy = fCopy.returnPoints(d);
         assertEquals(returnedCopy, returned);
         assertEquals(fCopy, f);
     }
